a70434113a8d4beca8e1d3a92391380e7754b6cf
Mohanad <info@mohnd-info.com>
2024-10-15 17:04:34 +0300

    init

---
+++ android/app/src/main/kotlin/com/mohndinfo/healthcare/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mohndinfo.healthcare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:healthcare/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
